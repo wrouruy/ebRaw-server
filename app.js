b91878c6cf839178847faf83894637d808c377f5
wroxx <wroyruy@gmail.com>
2025-11-22 12:27:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express();
 const PORT = 3000;
 
 app.get('/hello', (req, res) => {
-    res.json({ msg: 'hello, edRaw!' });
+    res.json({ msg: 'hello, ebRaw!' });
 })
 
 app.listen(PORT, (err) => {
     if(err) console.log(err);
     console.log('server has listen on port ' + PORT);
+
 })
